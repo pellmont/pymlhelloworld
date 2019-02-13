5c584fc64a2199cad87bf85cbb01c15b56a4d68c
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-02-13 19:19:47 +0100

    Serving static content from `model_info` directory.

--- pymlhelloworld/__init__.py
+++ pymlhelloworld/__init__.py
@@ -4,13 +4,23 @@ Hello World REST Application.
 Assembles all the REST endpoints.
 """
 
-from flask import Flask
+import os
+
+from flask import Flask, send_from_directory
 
 from .api import api
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 api.init_app(app)
+
+
+@app.route('/model_info/<path:path>')
+def model_info(path):
+    """Serve static files for model description."""
+    this_dir = os.path.dirname(__file__)
+    target_dir = os.path.join(this_dir, '..', 'model_info')
+    return send_from_directory(target_dir, path)
 
 
 if __name__ == '__main__':
