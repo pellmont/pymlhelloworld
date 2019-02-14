b04dec1416fc5a6b990b09caad1ab1a324bf69c8
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-02-14 16:39:53 +0100

    Simplify serving of static files.

--- pymlhelloworld/__init__.py
+++ pymlhelloworld/__init__.py
@@ -4,23 +4,15 @@ Hello World REST Application.
 Assembles all the REST endpoints.
 """
 
-import os
-
-from flask import Flask, send_from_directory
+from flask import Flask
 
 from .api import api
 
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__,
+            static_url_path='/model_info/',
+            static_folder='model_info')
 api.init_app(app)
-
-
-@app.route('/model_info/<path:path>')
-def model_info(path):
-    """Serve static files for model description."""
-    this_dir = os.path.dirname(__file__)
-    target_dir = os.path.join(this_dir, '..', 'model_info')
-    return send_from_directory(target_dir, path)
 
 
 if __name__ == '__main__':
