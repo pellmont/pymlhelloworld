6a873cb39cecb1de54a5e9c229070de554b26a05
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-02-15 18:19:41 +0100

    Healthcheck resource with tests.

--- web/pymlhelloworld/api/healthcheck.py
+++ web/pymlhelloworld/api/healthcheck.py
@@ -1,8 +1,30 @@
 """This module implements healthcheck API for monitoring."""
+# pragma: no cover
+from flask import abort, url_for
+
 from flask_restplus import Namespace, Resource
+
+import requests
 
 
 api = Namespace('healthcheck', description='Healthcheck operation')
+
+
+test_payload = {
+    'home_ownership': True,
+    'purpose': 'some purpose',
+    'addr_state': 'some state',
+    'loan_amnt': 1.0,
+    'installement': 1,
+    'annual_income': 1.0,
+    'int_rate': 1.0,
+    'emp_lenght': 1,
+}
+
+expected_response = {
+    'good_loan': True,
+    'confidence': 0.7,
+}
 
 
 @api.route('/')
@@ -15,4 +37,13 @@ class Health(Resource):
         Call services of this application and verify that all endpoints
         behave as expected.
         """
-        print("Healthcheck called!")
+        predict_ep = url_for('predict_predict', _external=True)
+        try:
+            r = requests.post(predict_ep, data=test_payload, timeout=0.5)
+        except requests.exceptions.Timeout:
+            abort(500)
+
+        if r.json() != expected_response:
+            abort(500)
+
+        return '', 200
--- web/tests/conftest.py
+++ web/tests/conftest.py
@@ -1,8 +1,17 @@
 """pytest configuration. E.g. fixtures for all tests."""
 
+import time
+from multiprocessing import Process
+
 import pytest
 
 from pymlhelloworld import app
+
+
+# To test with externally run flask instance set this to True
+DEBUG_STANDALONE = False
+
+PORT = 15000
 
 
 def pytest_addoption(parser):
@@ -22,3 +31,26 @@ def real_model(request):
 def client():
     """Client for testing REST calls."""
     return app.test_client()
+
+
+if DEBUG_STANDALONE:
+    @pytest.fixture(scope="session")
+    def flask_server():
+        """Use an external Flask instance."""
+        return 'http://localhost:5000'
+else:
+    @pytest.fixture(scope="session")
+    def flask_server():
+        """Create an instance of Flask server."""
+        def run_app(port):
+            app.run(port=port, use_reloader=False)
+
+        server_process = Process(target=run_app, args=(PORT, ))
+        server_process.start()
+
+        # Give 2 secs for the Flask server to start up
+        time.sleep(2)
+
+        yield f'http://localhost:{PORT}'
+
+        server_process.terminate()
